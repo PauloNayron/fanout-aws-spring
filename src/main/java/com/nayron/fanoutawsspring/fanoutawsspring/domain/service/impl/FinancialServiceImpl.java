cde205388f1b15ff1b9b8d53651433115e7e874c
PauloNayron <paulonayron.s@hotmail.com>
2022-09-14 02:40:14 -0300

    config: adds the published in the SNS and ombudsman of the Financial Service SQS

--- src/main/java/com/nayron/fanoutawsspring/fanoutawsspring/domain/service/impl/FinancialServiceImpl.java
+++ src/main/java/com/nayron/fanoutawsspring/fanoutawsspring/domain/service/impl/FinancialServiceImpl.java
@@ -5,13 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Slf4j
 @Component
 public class FinancialServiceImpl implements FinancialService {
 
-  @SqsListener(value = "https://sqs.us-east-1.amazonaws.com/950890890944/financial-purchase-create-sqs", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+  @SqsListener(value = "${sqs.financial-purchase}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
   private void receivePurchanseMessage(String message) {
     log.info("message receive {}", message);
   }
